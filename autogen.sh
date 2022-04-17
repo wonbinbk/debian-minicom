13dac9ce6a2db168288403951bc57ed9f39639ba
Adam Lackorzynski <adam@l4re.org>
2022-04-17 18:36:36 +0200

    autogen.sh: Copy files instead of linking
    Maybe we're copying some minicom sources around.

--- autogen.sh
+++ autogen.sh
@@ -9,5 +9,5 @@ set -x
 aclocal-$AUTOMAKEVER
 [ "$?" != 0 ] && echo "aclocal-$AUTOMAKEVER not available or failed!" && exit 1
 autoheader || exit 1
-automake-$AUTOMAKEVER --add-missing --force --gnu || exit 1
+automake-$AUTOMAKEVER -c --add-missing --force --gnu || exit 1
 autoconf || exit 1
